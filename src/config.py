a62f81b741aad07528b006ee342acf7c17539e4c
Kostov Ilya <kick1991@mail.ru>
2024-08-17 18:37:04 +0500

    feat: add settings, db connection

---
+++ src/config.py
@@ -0,0 +1,42 @@
+from dotenv import find_dotenv, load_dotenv
+from pydantic_settings import SettingsConfigDict
+
+load_dotenv(find_dotenv('.env'))
+
+
+class Settings:
+    """Settings class."""
+
+    MODE: str
+
+    DB_HOST: str
+    DB_PORT: int
+    DB_USER: str
+    DB_PASS: str
+    DB_NAME: str
+
+    REDIS_HOST: str
+    REDIS_PORT: int
+    REDIS_DB: int
+
+    naming_convention: dict[str, str] = {
+        'ix': 'ix_%(column_0_label)s',
+        'uq': 'uq_%(table_name)s_%(column_0_N_name)s',
+        'ck': 'ck_%(table_name)s_%(constraint_name)s',
+        'fk': 'fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s',
+        'pk': 'pk_%(table_name)s',
+    }
+
+    @property
+    def DB_URL(self) -> str:
+        return f'postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}'
+
+    @property
+    def REDIS_URL(self) -> str:
+        return f'redis://{self.REDIS_HOST}:{self.REDIS_PORT}/{self.REDIS_DB}'
+
+    model_config = SettingsConfigDict(env_file='.env')
+
+
+load_dotenv(find_dotenv('.env'))
+settings = Settings()
