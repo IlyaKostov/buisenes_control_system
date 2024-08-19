6bbf1464b9c2e8af978532a24b1374bba77b52e9
Kostov Ilya <kick1991@mail.ru>
2024-08-19 15:42:06 +0500

    feat: update user model

---
+++ src/models/__init__.py
@@ -0,0 +1,15 @@
+__all__ = [
+    'BaseModel',
+    'CompanyModel',
+    'UserModel',
+    'SecretModel',
+    'AccountModel',
+    'InviteModel'
+]
+
+from src.models.base import BaseModel
+from src.models.company import CompanyModel
+from src.models.user import UserModel
+from src.models.secret import SecretModel
+from src.models.account import AccountModel
+from src.models.invite import InviteModel
--- src/models/user.py
+++ src/models/user.py
@@ -1,10 +1,15 @@
-from sqlalchemy import String
-from sqlalchemy.orm import Mapped, mapped_column
+from typing import TYPE_CHECKING
+
+from sqlalchemy import String, ForeignKey, UniqueConstraint
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from src.models import BaseModel
 from src.models.mixins.company_mixin import CompanyMixin
 from src.schemas.user import UserDB
 from src.utils.custom_types import created_at, updated_at, uuid_pk
+
+if TYPE_CHECKING:
+    from src.models.account import AccountModel
 
 
 class UserModel(CompanyMixin, BaseModel):
@@ -18,6 +23,12 @@ class UserModel(CompanyMixin, BaseModel):
     middle_name: Mapped[str | None] = mapped_column(String(50), default=None)
     created_at: Mapped[created_at]
     updated_at: Mapped[updated_at]
+
+    account_id: Mapped[int] = mapped_column(ForeignKey('account.id'))
+
+    account: Mapped['AccountModel'] = relationship(back_populates='user', cascade="all, delete-orphan")
+
+    __table_args__ = (UniqueConstraint('account_id'),)
 
     def to_pydantic_schema(self) -> UserDB:
         return UserDB(**self.__dict__)
