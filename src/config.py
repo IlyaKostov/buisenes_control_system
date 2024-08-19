38e49b8bd2247b20e64422bb0488ab38bc6972b1
Kostov Ilya <kick1991@mail.ru>
2024-08-19 15:42:56 +0500

    fix: update config

--- src/config.py
+++ src/config.py
@@ -1,10 +1,22 @@
+from pathlib import Path
+
 from dotenv import find_dotenv, load_dotenv
-from pydantic_settings import SettingsConfigDict
+from pydantic import BaseModel
+from pydantic_settings import SettingsConfigDict, BaseSettings
 
 load_dotenv(find_dotenv('.env'))
 
+BASE_DIR = Path(__file__).parent.parent
 
-class Settings:
+
+class AuthJWT(BaseModel):
+    private_key_path: Path = BASE_DIR / "certs" / "jwt-private.pem"
+    public_key_path: Path = BASE_DIR / "certs" / "jwt-public.pem"
+    algorithm: str = "RS256"
+    access_token_expire_minutes: int = 1440
+
+
+class Settings(BaseSettings):
     """Settings class."""
 
     MODE: str
@@ -19,6 +31,8 @@ class Settings:
     REDIS_PORT: int
     REDIS_DB: int
 
+    auth_jwt: AuthJWT = AuthJWT()
+
     naming_convention: dict[str, str] = {
         'ix': 'ix_%(column_0_label)s',
         'uq': 'uq_%(table_name)s_%(column_0_N_name)s',
@@ -29,7 +43,7 @@ class Settings:
 
     @property
     def DB_URL(self) -> str:
-        return f'postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}'
+        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
 
     @property
     def REDIS_URL(self) -> str:
