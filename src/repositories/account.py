2fd939879fde19de717079ec335a66a3deba569f
Kostov Ilya <kick1991@mail.ru>
2024-08-26 00:20:51 +0500

    feat: add get methods to account repository

--- src/repositories/account.py
+++ src/repositories/account.py
@@ -1,5 +1,27 @@
+from sqlalchemy import select
+from sqlalchemy.orm import joinedload
+
+from src.models import AccountModel
 from src.utils.repository import SQLAlchemyRepository
 
 
 class AccountRepository(SQLAlchemyRepository):
-    model = 'account'
+    model = AccountModel
+
+    async def get_account_by_email(self, email: str):
+        stmt = (
+            select(self.model)
+            .options(joinedload(self.model.secret), joinedload(self.model.user), joinedload(self.model.invite))
+            .filter_by(email=email)
+        )
+        result = await self.session.execute(stmt)
+        return result.scalar_one_or_none()
+
+    async def get_account_by_id(self, _id: int):
+        stmt = (
+            select(self.model)
+            .options(joinedload(self.model.secret), joinedload(self.model.user), joinedload(self.model.invite))
+            .filter_by(id=_id)
+        )
+        result = await self.session.execute(stmt)
+        return result.scalar_one_or_none()
