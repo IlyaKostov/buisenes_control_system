6a3d4432d9e526843f1b832b2415914244b18201
Kostov Ilya <kick1991@mail.ru>
2024-09-03 15:08:28 +0500

    fix: add unique constraint

--- src/models/struct_adm_positions.py
+++ src/models/struct_adm_positions.py
@@ -1,6 +1,6 @@
 from uuid import uuid4
 
-from sqlalchemy import ForeignKey
+from sqlalchemy import ForeignKey, UniqueConstraint
 from sqlalchemy.orm import Mapped, mapped_column
 from src.models import BaseModel
 from src.utils.custom_types import uuid_pk
@@ -8,14 +8,17 @@ from src.utils.custom_types import uuid_pk
 
 class StructAdmPositionModel(BaseModel):
     __tablename__ = "struct_adm_position"
+    __table_args__ = (
+        UniqueConstraint('struct_adm_id', 'position_id', name='idx_unique_struct_adm_position'),
+    )
 
     id: Mapped[uuid_pk]
 
     struct_adm_id: Mapped[uuid4] = mapped_column(
-        ForeignKey("struct_adm.id", ondelete="CASCADE"), primary_key=True
+        ForeignKey("struct_adm.id", ondelete="CASCADE")
     )
 
     position_id: Mapped[uuid4] = mapped_column(
-        ForeignKey("position.id", ondelete="CASCADE"), primary_key=True
+        ForeignKey("position.id", ondelete="CASCADE")
     )
 
