c8c72a265d5b850be1b873a3fb2f91d342a3f743
Kostov Ilya <kick1991@mail.ru>
2024-09-03 15:09:13 +0500

    fix: upgrade init with new models

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -4,7 +4,14 @@ __all__ = [
     'UserModel',
     'SecretModel',
     'AccountModel',
-    'InviteModel'
+    'InviteModel',
+    'PositionModel',
+    'StructAdmModel',
+    'StructAdmPositionModel',
+    'TaskModel',
+    'task_observers',
+    'task_assignees',
+    'UserPositionModel'
 ]
 
 from src.models.base import BaseModel
@@ -13,3 +20,9 @@ from src.models.user import UserModel
 from src.models.secret import SecretModel
 from src.models.account import AccountModel
 from src.models.invite import InviteModel
+from src.models.position import PositionModel
+from src.models.struct_adm import StructAdmModel
+from src.models.struct_adm_positions import StructAdmPositionModel
+from src.models.task import TaskModel
+from src.models.user_tasks_associations import task_assignees, task_observers
+from src.models.users_positions import UserPositionModel
