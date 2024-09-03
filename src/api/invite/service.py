7ecbed5c04e06e29f1ed6ab0dd43ab2d45da511d
Kostov Ilya <kick1991@mail.ru>
2024-09-03 15:04:34 +0500

    fix: add background task for sending email

--- src/api/invite/service.py
+++ src/api/invite/service.py
@@ -1,6 +1,8 @@
 import random
 import string
 
+from fastapi import BackgroundTasks
+
 from src.models import InviteModel, AccountModel
 from src.utils.base_service import BaseService
 from src.utils.send_email import sending_email_with_invite_code
@@ -11,23 +13,23 @@ class InviteService(BaseService):
     base_repository = 'invite'
 
     @transaction_mode
-    async def create_invite(self, account: AccountModel) -> InviteModel:
+    async def create_invite(self, account: AccountModel, background_tasks: BackgroundTasks) -> InviteModel:
         invite_token: str = await self.generate_invite()
 
         invite: InviteModel = await self.uow.invite.add_one_and_get_obj(account_id=account.id, token=invite_token)
 
-        # await sending_email_with_invite_code(account.email, invite_token)
+        background_tasks.add_task(sending_email_with_invite_code, account.email, invite_token)
 
         return invite
 
     @transaction_mode
-    async def update_invite(self, account: AccountModel) -> InviteModel:
+    async def update_invite(self, account: AccountModel, background_tasks: BackgroundTasks) -> InviteModel:
         new_token: str = await self.generate_invite()
         update_values: dict[str, str] = {'token': new_token}
 
         invite: InviteModel = await self.uow.invite.update_one_by_id(account.invite.id, update_values)
 
-        # await sending_email_with_invite_code(account.email, new_token)
+        background_tasks.add_task(sending_email_with_invite_code, account.email, new_token)
 
         return invite
 
