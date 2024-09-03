863da2e31e2b7b79aea0631acdddd66d88656148
Kostov Ilya <kick1991@mail.ru>
2024-09-03 15:11:30 +0500

    fix: add sequence

--- src/utils/custom_types.py
+++ src/utils/custom_types.py
@@ -3,12 +3,14 @@ from datetime import datetime
 from typing import Annotated, Any
 from uuid import uuid4
 
-from sqlalchemy import UUID, DateTime, Integer, text
+from sqlalchemy import UUID, DateTime, Integer, text, Sequence
 from sqlalchemy.orm import mapped_column
 
 async_func = Callable[..., Awaitable[Any]]
 
-integer_pk = Annotated[int, mapped_column(Integer, primary_key=True)]
+id_seq = Sequence('struct_adm_id_seq')
+
+integer_pk = Annotated[int, mapped_column(Integer, primary_key=True, server_default=id_seq.next_value())]
 uuid_pk = Annotated[uuid4, mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid4)]
 
 dt_now_utc_sql = text("TIMEZONE('utc', now())")
